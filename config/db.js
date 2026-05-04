6953604f682e97111223ece46ccc952524ca6331
Nakum Meet <nakummeet3570@gmail.com>
2026-05-05 00:14:55 +0530

    fix db connection for vercel

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = db.connections[0].readyState;
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.error("DB Error:", error.message);
+    // ❌ REMOVE process.exit
   }
 };
 
